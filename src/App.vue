719193b5ac8b124abaa84c2eb652e1c15d16a432
joshuaward <joshua.ward@me.com>
2021-01-22 19:00:11 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,574 @@
+<template lang="pug">
+	#app.game
+		input(type="hidden" value="/data/flags.json")
+		header.game__header
+			div Correct: #[span {{ answersCorrect }} / {{ flags.length }}]
+			.game__title {{ title }}
+			div Incorrect: #[span {{ answersIncorrect }}]
+		
+		transition-group(name="section" tag="main")
+			//- start screen w/ play button
+			section.game__panel.game__panel--start(v-if="start", key="1")
+				.game__panel-inner
+					h1.game__panel-title {{ title }}
+					p.game__panel-intro Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					button.game__panel-button(@click="getFlag" type="button") Start
+					
+					
+			//- guessing screen w/ flag, input, and submit button
+			section.game__panel.game__panel--game(v-else-if="game", key="2")
+				.game__panel-inner
+					header.game__panel-header
+						h2.game__panel-question What Country's Flag is this?
+					.game__panel-flag
+						img(:src="gameCorrect.flag")
+					.game__panel-form
+						.game__panel-answer(v-for="(item,i) in gameFlags" :key="i")
+							input(type="radio" :id="[`answer--${i}`]" :value="item.name" name="answers")
+							label(v-bind:for="[`answer--${i}`]") {{ item.name }}
+					.game__panel-footer
+						button.game__panel-submit(@click.prevent="checkAnswer" type="button") Check Answer
+
+				.game__panel-error(v-if="error") Please make a selection!
+
+
+			//- correct answer panel
+			section.game__panel.game__panel--result(v-else-if="correctAnswer", key="3")
+				.game__panel-inner
+					header.game__panel-header
+						h2.game__panel-question.correct #[strong #[span {{ gameCorrect.name }}]] is CORRECT!
+					.game__panel-correct
+						img(:src="gameCorrect.flag")
+					button.game__panel-button(@click="getFlag" type="button") Next Flag
+
+
+			//- incorrect answer panel
+			section.game__panel.game__panel--result(v-else-if="incorrectAnswer", key="4")
+				.game__panel-inner
+					header.game__panel-header
+						h2.game__panel-question.incorrect Inorrect! The correct answer is #[span {{ gameCorrect.name }}]
+					.game__panel-correct
+						img(:src="gameCorrect.flag")
+					button.game__panel-button(@click="getFlag" type="button") Next Flag
+
+
+			//- results screen w/ next flag button
+			section.game__panel(v-else, key="5")
+				.game__panel-inner
+					p END!
+
+
+	//- 
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+	name: 'App',
+	data() {
+		return {
+			title: 'Flag Guessing Game',
+			flags: [],
+			start: true,
+			error: false,
+			game: false,
+			gameFlags: [],
+			gameCorrect: null,
+			correctAnswer: false,
+			incorrectAnswer: false,
+			answersCorrect: 0,
+			answersIncorrect: 0
+		}
+	},
+	methods: {
+		getData() {
+			axios.get('https://restcountries.eu/rest/v2/all?fields=name;flag')
+				.then(res => {
+					this.flags = res.data;
+					console.log('Flags: ', this.flags)
+				})
+		},
+		shuffleAnswers(array) {
+			var currentIndex = array.length;
+			var temporaryValue, randomIndex;
+			// While there remain elements to shuffle...
+			while (0 !== currentIndex) {
+				// Pick a remaining element...
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex -= 1;
+				// And swap it with the current element.
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+			}
+			return array;			
+		},
+		getFlag() {
+			let correct = Math.floor(Math.random() * this.gameFlags.length) + 1;	
+			
+			for(let i = 0; i < 4; i++) {
+				let answers = Math.floor(Math.random() * this.flags.length) + 1;
+				this.gameFlags.push(this.flags[answers]);
+			}
+
+			let index = this.gameFlags.indexOf('undefined');
+			console.log(index)
+			if(index > -1) {
+				this.gameFlags.splice(index, 1);
+				console.log('spliced!');
+			} else {
+				console.log('Cool beans!')
+			}
+
+			if(this.flags < this.gameFlags) {
+				this.game = false;
+			}
+			this.correctAnswer = false;
+			this.incorrectAnswer = false;
+			this.start = false;
+			this.game = true;
+			this.gameCorrect = this.gameFlags[correct];
+			this.shuffleAnswers(this.gameFlags);
+
+			console.log(this.flags)
+		},
+		checkAnswer() {
+			const userGuess = this.$el.querySelector('.game__panel-answer input:checked');
+			const userGuessVal = userGuess.value;
+			if (this.gameCorrect.name.localeCompare(userGuessVal) == 0) {
+				this.correctAnswer = true;
+				this.game = false;
+				this.gameFlags = [];
+				this.answersCorrect += 1;
+				const index = this.flags.indexOf(this.gameCorrect)
+				this.flags.splice(index, 1);
+			} else {
+				this.incorrectAnswer = true;
+				this.game = false;
+				this.gameFlags = [];
+				this.answersIncorrect += 1;
+			}
+		}
+	},			
+	created() {
+		this.getData();
+		console.log(this.flags);
+	}
+}
+
+// [
+// 	{
+// 		"flag": "https://restcountries.eu/data/afg.svg",
+// 		"name": "Afghanistan"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/ala.svg",
+// 		"name": "Åland Islands"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/alb.svg",
+// 		"name": "Albania"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/dza.svg",
+// 		"name": "Algeria"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/asm.svg",
+// 		"name": "American Samoa"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/and.svg",
+// 		"name": "Andorra"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/ago.svg",
+// 		"name": "Angola"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/aia.svg",
+// 		"name": "Anguilla"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/ata.svg",
+// 		"name": "Antarctica"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/atg.svg",
+// 		"name": "Antigua and Barbuda"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/arg.svg",
+// 		"name": "Argentina"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/arm.svg",
+// 		"name": "Armenia"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/abw.svg",
+// 		"name": "Aruba"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/aus.svg",
+// 		"name": "Australia"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/aut.svg",
+// 		"name": "Austria"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/aze.svg",
+// 		"name": "Azerbaijan"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/bhs.svg",
+// 		"name": "Bahamas"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/bhr.svg",
+// 		"name": "Bahrain"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/bgd.svg",
+// 		"name": "Bangladesh"
+// 	},
+// 	{
+// 		"flag": "https://restcountries.eu/data/brb.svg",
+// 		"name": "Barbados"
+// 	}
+// ],
+</script>
+
+<style lang="scss">
+// timing 
+$base-duration: 250ms;
+
+// Colors
+$color1: #1C2321;
+$color2: #4E5166;
+$color3: #7D98A1;
+$color4: #70A9A1;
+$color5: #FCAF58;
+
+$success: #27ae60;
+$info: #3498db;
+$warning: #f1c40f;
+$danger: #e74c3c;
+
+$white: #fff;
+$black: #000;
+$gray: #ecf0f1;
+$darkGray: #7f8c8d;
+$whitesmoke: whitesmoke;
+$text: #555;
+
+// Breakpoints
+$sm: 20rem;
+$med: 48rem;
+$lg: 64rem;
+
+// sizes 
+$max-width: 1200px;
+$spacer: 1rem;
+
+// fonts / icons
+$roboto: 'Roboto', sans-serif;
+$robotoCondensed: 'Roboto Condensed', sans-serif;
+$awesome: "Font Awesome 5 Free";
+$awesomeWeight: 900;
+
+*, *:before, *:after {
+	box-sizing: border-box;
+	outline: none;
+}
+
+html {
+	font-family: $roboto;
+	font-size: 16px;
+	font-smooth: auto;
+	font-weight: 300;
+	line-height: 1.5;
+	color: #444;
+}
+
+body {
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 100vh;
+	margin: 0;
+}
+
+#app {
+	display: flex;
+	flex-direction: column;
+	width: 100vw;
+	height: 100vh;
+}
+
+.game {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: rgba($black,0.7);
+	&::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-attachment: fixed;
+		background-image: url('https://images.pexels.com/photos/4468974/pexels-photo-4468974.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+		background-position: center center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		z-index: -1;
+	}
+	&__title {
+		// color: $white;
+	}
+	&__header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 20px;
+		background-color: $white;
+		div {
+			font-weight: bold;
+			&:first-child {
+				span {
+					color: $success;
+				}
+			}
+			&:last-child {
+				span {
+					color: $danger;
+				}
+			}
+		}
+	}
+	&__panel {		
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		display: block;
+		width: 420px;
+		margin: 20px;
+		padding: 70px 40px 40px;
+		background-color: $white;
+		border-radius: 10px;
+		box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+		transform: translate(-50%,-50%);
+		z-index: 1;
+		// @media(min-width: 768px) {
+		// 	width: 375px;
+		// }
+		&--start {
+			padding: 40px;
+		}
+		&--result {
+			padding-top: 80px;
+		}
+		&-inner {
+			display: flex;
+			flex-direction: column;
+		}
+		&-button {
+			display: inline-flex;
+			align-items: center;
+			justify-content: center;
+			margin-left: auto;
+			padding: 10px 30px;
+			background-color: $color2;
+			border: 0;
+			border-radius: 99px;
+			color: $white;
+			transition: 0.25s ease;
+			cursor: pointer;
+			&::after {
+				content: '\f061';
+				margin-left: 10px;
+				font-family: $awesome;
+				font-weight: 900;
+				transform: translateX(0);
+				transition: 0.25s ease;
+			}
+			&:hover {
+				background-color: $color4;
+				&::after {
+					transform: translateX(10px);
+				}
+			}
+		}
+		&-question {
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			margin-top: 0;
+			padding: 10px;
+			background-color: rgba($darkGray, 0.4);
+			border-radius: 10px 10px 0 0;
+			color: $text;
+			font-size: 20px;
+			font-weight: bold;
+			text-align: center;
+			&.correct {
+				span {
+					color: $success;
+					font-weight: bold;
+					text-shadow: 1px 1px 0 darken($success,9%);
+				}
+			}
+			&.incorrect {
+				span {
+					color: $danger;
+					font-weight: bold;
+				}
+			}
+		}
+		&-correct,
+		&-flag {
+			position: relative;
+			display: block;
+			margin-bottom: 1.875rem;
+			&::after {
+				content: '';
+				display: block;
+				padding-top: 66.6666%;
+			}
+			img {
+				position: absolute;
+				top: 0;
+				bottom: 0;
+				left: 0;
+				right: 0;
+				width: 100%;
+				height: 100%;
+				object-fit: cover;
+				max-width: 100%;
+				box-shadow: 0 0 10px rgba($black,0.2);
+			}
+		}
+		&-form {
+			display: flex;
+			flex-direction: column;
+			margin-bottom: 1.875rem;
+		}
+		&-answer {
+			flex: 0 0 auto;
+			margin-bottom: 2px;
+			input {
+				display: none;
+
+				&:checked ~ label {
+					background-color: $color2;
+					color: $white;
+					&:before {
+						content: '\f00c';
+						background-color: rgba($white,0.2);
+						color: $success;
+						font-family: $awesome;
+					}
+				}
+			}
+			label {
+				display: flex;
+				align-items: center;
+				padding: 5px 15px;
+				background-color: $whitesmoke;
+				border-radius: 5px;
+				font-size: 16px;
+				font-weight: bold;
+				transition: 0.25s ease;
+				cursor: pointer;
+				&:hover {
+					background-color: rgba($color2,0.5);
+					color: $white;
+					&::before {
+						border-color: $white;
+					}
+				}
+				&::before {
+					content: '';
+					flex: 1 0 20px;
+					display: inline-flex;
+					align-items: center;
+					justify-content: center;
+					max-width: 20px;
+					height: 20px;
+					margin-right: 10px;
+					border: 2px solid $color2;
+					border-radius: 4px;
+					font-size: 12px;
+				}
+			}
+		}
+		&-footer {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+		&-submit {
+			display: inline-flex;
+			align-items: center;
+			justify-content: center;
+			padding: 10px 30px;
+			background-color: $color3;
+			border: 0;
+			border-radius: 99px;
+			box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+			transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+			color: $white;
+			cursor: pointer;
+			&:hover {
+				background-color: $success;
+				box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+			}
+			&:hover {
+				&::after {
+					width: 20px;
+					margin-left: 5px;
+					transform: scale(1);
+				}
+			}
+			&::after {
+				content: '\f00c';
+				width: 0;
+				transform: scale(0);
+				transition: 0.25s ease;
+				font-family: $awesome;
+				font-weight: 900;
+			}
+		}
+	}
+}
+
+.section-enter {
+	transform: translate(-300%,-50%);
+}
+
+.section-enter-active {
+	transition: 0.5s ease-in-out;
+}
+
+.section-leave-active {
+	transition: 0.5s ease-in-out;
+}
+
+.section-leave-to {
+	transform: translate(300%,-50%);
+}
+</style>
